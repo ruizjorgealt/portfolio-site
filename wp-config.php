1f358ebbab196c0f728098fe61601177497f705c
Jorge A. Ruiz <39965509+ruizjorgealt@users.noreply.github.com>
2018-12-10 00:22:09 -0800

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -23,13 +23,13 @@
 define('DB_NAME', 'portfolio');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'portfolio@web110');
+define('DB_PASSWORD');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
