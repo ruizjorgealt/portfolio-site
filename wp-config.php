d8a35115141f346fe44e82e4eb2868445c018121
Jorge A. Ruiz <39965509+ruizjorgealt@users.noreply.github.com>
2018-12-10 00:20:55 -0800

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'portfolio');
+define('DB_NAME');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'portfolio@web110');
+define('DB_PASSWORD');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
